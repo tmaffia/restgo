52f47e792569cf05f177079abb2b7ec69c4e25d8
Tom Maffia <tmaffia@tbscg.com>
2024-07-18 17:21:55 -0400

    Formatting

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,18 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/sirupsen/logrus"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
 	"github.com/tmaffia/pkg/handlers"
 )
-
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", handlers.HomeHandler)
 	r.Get("/health", handlers.HealthCheckHandler)
+
 
 	log.Println("API is running!")
 	if err := http.ListenAndServe(":8080", r); err != nil {
